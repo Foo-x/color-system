1a4df76d4f8e86b75e811dd98e3a57b75a7ff9a2
Foo-x <www.sand.x@gmail.com>
2020-07-05 19:02:06 +0900

    Fix HEX value

--- src/entities/spaces/HEX.ts
+++ src/entities/spaces/HEX.ts
@@ -1,9 +1,15 @@
-const HEXPattern = /^#?([a-f\d]{6}|[a-f\d]{3})$/i;
+const SHORT_HEX_PATTERN = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+const HEX_PATTERN = /^#?([a-f\d]{6})$/i;
 
 export type HEX = string & {
   readonly _HEXBrand: unique symbol;
 };
 
 export const toHEX = (s: string): HEX | undefined => {
-  return HEXPattern.test(s) ? (s as HEX) : undefined;
+  const hex = s.replace(
+    SHORT_HEX_PATTERN,
+    (_, r, g, b) => r + r + g + g + b + b
+  );
+  const result = HEX_PATTERN.exec(hex);
+  return result ? (`#${result[1]}` as HEX) : undefined;
 };
