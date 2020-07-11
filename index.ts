fb93d7b2202f90961c6a40602e5925610a650e51
Foo-x <www.sand.x@gmail.com>
2020-07-12 01:28:08 +0900

    Add index

---
+++ index.ts
@@ -0,0 +1,10 @@
+export * as ColorSpaceConverter from "./src/entities/spaces/ColorSpaceConverter";
+export * as HEX from "./src/entities/spaces/HEX";
+export * as HSL from "./src/entities/spaces/HSL";
+export * as RGB from "./src/entities/spaces/RGB";
+
+export * as PCCS from "./src/entities/systems/PCCS";
+
+export * as Degree from "./src/entities/units/Degree";
+export * as Percent from "./src/entities/units/Percent";
+export * as UInt8 from "./src/entities/units/UInt8";
